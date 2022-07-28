09fd1e8e1ba3d40a12c186b2204712f2a49c6cd4
gelson2021 <gelson23tg@gmail.com>
2022-07-28 16:19:25 +0100

    logi
    add caminho correcto

--- Agenda_projecto/APP/Back_end/validar_Login.js
+++ Agenda_projecto/APP/Back_end/validar_Login.js
@@ -1,22 +1,25 @@
 const express = require('express')
-const session = require('express-session')
+    // const session = require('express-session')
 
 var path = require('path');
 const app = express();
 
 app.engine('html', require('ejs').renderFile);
-app.use(express.json());
-app.use('/public', express.static(path.join(__dirname)));
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, '/views'));
-app.use(express.urlencoded({ extended: true }));
+app.use('/public', express.static(path.join(__dirname)));
+app.use(express.json());
+// app.use('/public', express.static(path.join(__dirname)));
+// app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '../views'));
+// app.use(express.urlencoded({ extended: true }));
 
 app.post('/', (req, res) => {
 
 })
 
 app.get('/', (req, res) => {
-    res.sendFile('view.login.ejs')
+    // res.sendFile('view.login.ejs')
+    res.render('login')
 })
 
 app.listen(3000, () => {
